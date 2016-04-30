dfc0fd8523325eaf1e2b35386b70289f214778f8
Peter Doherty <pdoherty926@gmail.com>
2016-04-30 11:44:36 -0400

    tweaking whitespace/commas in Model

--- Model.elm
+++ Model.elm
@@ -1,24 +1,24 @@
 module Model(GameObject, initialGameObject) where
 
 type alias GameObject = {
-  initials : String,
-  initialsSaved : Bool,
-  answer : String,
-  options : List String,
-  guess : Maybe String,
-  score : Int,
-  rounds : Int,
-  gameOver : Bool
-}
+    initials : String
+  , initialsSaved : Bool
+  , answer : String
+  , options : List String
+  , guess : Maybe String
+  , score : Int
+  , rounds : Int
+  , gameOver : Bool
+  }
 
 initialGameObject : GameObject
 initialGameObject = {
-    initials = "",
-    initialsSaved = False,
-    answer = "rgb(0, 0, 0)",
-    options = ["rgb(0, 0, 0)"],
-    guess = Nothing,
-    score = 0,
-    rounds = 0,
-    gameOver = False
+    initials = ""
+  , initialsSaved = False
+  , answer = "rgb(0, 0, 0)"
+  , options = ["rgb(0, 0, 0)"]
+  , guess = Nothing
+  , score = 0
+  , rounds = 0
+  , gameOver = False
   }
